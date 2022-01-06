30f859af04d8e395629493d76701b253daa1b0f0
paravmahajan03 <paravmahajan03@gmail.com>
2022-01-06 15:14:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "weather-icons/css/weather-icons.css";
+import Weather from "./app_components/weather.component";
+import { render } from "@testing-library/react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_key = "d8b9a50daab604b9069d76934993c873";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.getWeather();
+  }
+  getWeather = async () => {
+    const api_call = await fetch(
+      `api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_key}`
+    );
+    const response = await api_call.json();
+    console.log(response);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Weather />
+      </div>
+    );
+  }
 }
 
 export default App;
